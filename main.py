a371abfb02ea12a8d8295a0c35a3461af3af65c0
closeplanet2 <68482902+closeplanet2@users.noreply.github.com>
2023-01-26 21:44:26 +0000

    Init Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from UI.PluginRequestUI import PluginRequestUI
+from Data.BukkitRequestData import BukkitRequestData
+
+
+plugin_request_ui = PluginRequestUI(
+    BukkitRequestData(
+        "https://bukkit.org/forums/plugin-requests.96/page-", 227, 10
+    )
+)
+
